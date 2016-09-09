201e8f040fe5ec01a1ac37925530700f4c4255da
brunops96 <brunops96@gmail.com>
2016-09-09 16:39:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pdi.c"
-
+/*
+  BRUNO PEDROSO SCHMOCKEL
+  CELSO LUIZ CHANE JUNIOR
+*/
 #define INPUT_IMAGE "a01 - Original.bmp"
 #define INPUT_CANAIS 1
 #define JANELA_A 3
 #define JANELA_L 3
 
+//Filtro ingenuo
 void filtroIng(){
     int i,x,y;
     int Y,X;
@@ -17,19 +21,19 @@ void filtroIng(){
         printf ("Erro abrindo a imagem.\n");
         exit (1);
     }
-    Imagem* img_out = criaImagem (img->largura, img->altura, img->n_canais);
-    for(i=0;i<img_out->n_canais;i++)
-        for(y=0;y<img->altura;y++)
-            for(x=0;x<img->largura;x++)
-                img_out->dados[i][y][x]=0;
-                
+    //percorre a imagem          
     for(i=0;i<img->n_canais;i++){
         for(y=(JANELA_A-1)/2;y<img->altura-(JANELA_A-1)/2;y++){
             for(x=(JANELA_L-1)/2;x<img->largura-(JANELA_L-1)/2;x++){
                 soma=0;
-                for(Y=y-JANELA_A/2;Y<y+JANELA_A/2;Y++)
-                    for(X=x-JANELA_L/2;X<x+JANELA_L/2;X++)
-                        soma+=img->dados[i][Y][X];
+                //Preenche bordas
+                if(y<y-(JANELA_A/2)-1 || y>(y+JANELA_A/2)+1 || x<x-(JANELA_L/2)-1 || x>(x+JANELA_L/2)+1)
+                    img_out->dados[i][y][x]=0;
+                //JANELA DESLIZANTE
+                else
+                    for(Y=y-JANELA_A/2;Y<=y+JANELA_A/2;Y++)
+                        for(X=x-JANELA_L/2;X<=x+JANELA_L/2;X++)
+                            soma+=img->dados[i][Y][X];
                 img_out->dados[i][y][x]=soma/(JANELA_A*JANELA_L);
             }
         }
