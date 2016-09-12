ed6a90db7282f8d95cee5e4e3085f554aacad888
celsolcj <celsochanejr@outlook.com>
2016-09-12 17:46:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,15 +8,14 @@
 #define INPUT_IMAGE "a01 - Original.bmp"
 #define INPUT_CANAIS 1
 //Altura e largura dos exemplos parecem invertidas, com exceção do 21x51
-#define JANELA_A 21
-#define JANELA_L 51
-#define JANELAINT_A 3
-#define JANELAINT_L 13
+#define JANELA_A 21//Altura da janela para filtro ingenup
+#define JANELA_L 51//Largura da imagem para filtro ingenuo
+#define JANELAINT_A 3//Altura da imagem para filtro integral
+#define JANELAINT_L 13//Largura da imagem para filtro integral
 
 void filtroIng(){
     int i,x,y;
     int Y,X;
-    float soma=0;
     Imagem* img = abreImagem (INPUT_IMAGE, INPUT_CANAIS);
     if (!img)
     {
@@ -24,22 +23,22 @@ void filtroIng(){
         exit (1);
     }
     Imagem* img_out = criaImagem (img->largura, img->altura, img->n_canais);
-    for(i=0;i<img_out->n_canais;i++)
+    for(i=0;i<img_out->n_canais;i++)//Preenche saida com zeros. Faz a borda ficar preta
         for(y=0;y<img->altura;y++)
             for(x=0;x<img->largura;x++)
                 img_out->dados[i][y][x]=0;
+    
+  //preenche a imagem de saida onde a janela cabe
     for(i=0;i<img->n_canais;i++){
         for(y=(JANELA_A-1)/2;y<img->altura-(JANELA_A-1)/2;y++){
             for(x=(JANELA_L-1)/2;x<img->largura-(JANELA_L-1)/2;x++){
-                soma=0;
                 for(Y=y-((JANELA_A-1)/2);Y<=y+((JANELA_A-1)/2);Y++)
                     for(X=x-((JANELA_L-1)/2);X<=x+((JANELA_L-1)/2);X++)
-                        soma+=img->dados[i][Y][X];
-                img_out->dados[i][y][x]=soma/(JANELA_A*JANELA_L);
+                        img_out->dados[i][y][x]+=img->dados[i][Y][X];
+                img_out->dados[i][y][x]/=(JANELA_A*JANELA_L);
             }
         }
     }
-    printf("hsuahua");
     salvaImagem(img_out,"saidaIng.bmp");
 }
 
@@ -53,58 +52,63 @@ void filtroInt(){
         exit (1);
     }
     Imagem* img_out = criaImagem (img->largura, img->altura, img->n_canais);
-    altura_int =img->altura+(JANELAINT_A/2)*2+1;
-    largura_int =img->largura+(JANELAINT_L/2)*2+1;
+    altura_int =img->altura+(JANELAINT_A/2)*2+1;//A imagem integral tem a altura da imagem + (JANELAINT_A/2)+1 para a esquerda(Borda com posição extra para fora da janela) e (JANELAINT_A/2)+1 para a direita (Borda necessária)
+    largura_int =img->largura+(JANELAINT_L/2)*2+1;//A imagem integral tem a largura da imagem + (JANELAINT_A/2)+1 para cima (Borda com posição extra para fora da janela) e (JANELAINT_A/2)+1 para a baixo (Borda necessária)
     Imagem* buff = criaImagem(largura_int,altura_int,img->n_canais);
-    for(i=0;i<img_out->n_canais;i++)
+    
+    for(i=0;i<img_out->n_canais;i++)//isso provavelmente é inútil, deixei por medo de tirar mesmo
         for(y=0;y<img->altura;y++)
             for(x=0;x<img->largura;x++){
                 img_out->dados[i][y][x]=0;
                 buff->dados[i][y][x]=0;
             }
+    
+  //Preenchimento da imagem integral com borda de azulejo        
     for(i=0;i<img->n_canais;i++){
-        yI=0;
-        for(y=-(JANELAINT_A)/2-1;y<altura_int-(JANELAINT_A)/2-1;y++){
-            xI=0;
-            for(x=-(JANELAINT_L)/2-1;x<largura_int-(JANELAINT_L)/2-1;x++){
-                if(xI==0 && yI==0){
-                    if(y<0)
+        yI=0;//Posição Y da imagem integral
+        for(y=-(JANELAINT_A)/2-1;y<altura_int-(JANELAINT_A)/2-1;y++){/*Loop que incrementa altura na imagem original. Quando y é negativo quer dizer que estamos na borda de cima da imagem(y+img->altura para azulejo),
+                                                                       e quando ele é maior que img->altura quer dizer que estamos na borda de baixo(y%img->altura para azulejo))*/
+            xI=0;//Posição X da imagem integral
+            for(x=-(JANELAINT_L)/2-1;x<largura_int-(JANELAINT_L)/2-1;x++){/*Loop que incrementa largura na imagem original. Quando x é negativo quer dizer que estamos na borda da esquerda da imagem(x+img->largura para azulejo),
+                                                                            e quando ele é maior que img->largura quer dizer que estamos na borda da direita(x%img->largura para azulejo)*/
+                if(xI==0 && yI==0){//primeira posição
+                    if(y<0)//Se a janela sai da tela(Precaução)
                         buff->dados[i][yI][xI]=img->dados[i][y+img->altura][x+img->largura];
                     else
                         buff->dados[i][yI][xI]=img->dados[i][y%img->altura][x%img->largura];
                 }
-                else if(yI==0) {
-                    if(x<0){
-                        if(y<0)
+                else if(yI==0) {//Se estamos na primeira linha da imagem integral
+                    if(x<0){//Se a janela sai da tela para a esquerda
+                        if(y<0)//Se a janela sai da tela para cima (talvez isso seja inutil na verdade, eu fiz isso meio que no piloto automático)
                             buff->dados[i][yI][xI]=buff->dados[i][yI][xI-1]+img->dados[i][y+img->altura][x+img->largura];
                         else
                             buff->dados[i][yI][xI]=buff->dados[i][yI][xI-1]+img->dados[i][y%img->altura][x+img->largura];
                     }
-                    else if(y<0)
+                    else if(y<0)//Se a janela só sai da tela para cima
                         buff->dados[i][yI][xI]=buff->dados[i][yI][xI-1]+img->dados[i][y+img->altura][x%img->largura];
                     else
                         buff->dados[i][yI][xI]=buff->dados[i][yI][xI-1]+img->dados[i][y%img->altura][x%img->largura];
                 }
-                else if(xI==0) {
-                    if(x<0){
-                        if(y<0)
+                else if(xI==0) {//Se estamos na primeira coluna da imagem integral
+                    if(x<0){//Se a janela sai da tela para a esquerda (talvez isso seja inutil na verdade, eu fiz isso meio que no piloto automático)
+                        if(y<0)//Se a janela sai da tela para cima 
                             buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+img->dados[i][y+img->altura][x+img->largura];
                         else
                             buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+img->dados[i][y%img->altura][x+img->largura];
                     }
-                    else if(y<0)
+                    else if(y<0)//Se a janela sai da tela somente para cima
                         buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+img->dados[i][y+img->altura][x%img->largura];
                     else
                         buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+img->dados[i][y%img->altura][x%img->largura];
                 }
-                else{
-                    if(x<0){
-                        if(y<0)
+                else{//Se estamos no meio da imagem integral
+                    if(x<0){//Se a janela sai da tela para a esquerda
+                        if(y<0)//Se a janela sai da tela para cima
                             buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+buff->dados[i][yI][xI-1]-buff->dados[i][yI-1][xI-1]+img->dados[i][y+img->altura][x+img->largura];
                         else
                             buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+buff->dados[i][yI][xI-1]-buff->dados[i][yI-1][xI-1]+img->dados[i][y%img->altura][x+img->largura];
                     }
-                    else if(y<0)
+                    else if(y<0)//Se a janela sai da tela somente para cima
                         buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+buff->dados[i][yI][xI-1]-buff->dados[i][yI-1][xI-1]+img->dados[i][y+img->altura][x%img->largura];
                     else
                         buff->dados[i][yI][xI]=buff->dados[i][yI-1][xI]+buff->dados[i][yI][xI-1]-buff->dados[i][yI-1][xI-1]+img->dados[i][y%img->altura][x%img->largura];
@@ -114,11 +118,13 @@ void filtroInt(){
             yI++;
         }
     }
+    
+  //Percorre imagem integal(buff) nas posições correspondentes à imagem original sem borda e realisa o cauculo da média com as propriedades da imagem integral
     for(i=0;i<img->n_canais;i++){
         for(y=0;y<img->altura;y++){
             for(x=0;x<img->largura;x++){
-                yI=y+JANELAINT_A/2+1;
-                xI=x+JANELAINT_L/2+1;
+                yI=y+JANELAINT_A/2+1;//Posição correspondente de y na imagem original sem borda na imagem integral
+                xI=x+JANELAINT_L/2+1;//Posição correspondente de x na imagem original sem borda na imagem integral
                 /*printf("%d %d ",yI,xI);
                 printf("%f.2 ",buff->dados[i][yI+JANELAINT_A/2][xI+JANELAINT_L/2]);
                 printf("%f.2 ",buff->dados[i][yI+JANELAINT_A/2][xI-JANELAINT_L/2-1]);
